2142da36eea4a30c466e7d4381480d009ac9d9c1
Vfrid <113192881+Vfrid@users.noreply.github.com>
2022-12-14 10:55:15 -0500

    Update plan.py

--- plan.py
+++ plan.py
@@ -1,6 +1,7 @@
 # open to home screen
 # ask user their username 
-# list 3 options as cards for games: foxes, dogs, ducks 
+# list 3 optio
+# ns as cards for games: foxes, dogs, ducks 
 # when user clicks on a game send them to a diff page w a random picture of animal (animal=which game they picked) and 10 'empty' stars
 # have user select however many stars they rank animal and then button 'submit'
 # once user selects 'submit' repeat process
@@ -10,4 +11,15 @@
 # show 'back to home' button
 # when user selects 'back to home' button, send them back to the three cards. if they click on animal they just played, show an error page (ex: already played! choose anothe one or log out)
 # if they choose another one repeat the process
+#take the top 3 animals from each users selection and save them to a database (list?????????) so that the next user will see not just their results, but the average results of theirs and everyone else who came before
 # if they log out go back to username input screen
+
+
+#plan option 2:
+#ask user their username
+#show one card named 'dog ranker'
+#when user clicks on 'begin' button on the card they are taken to another page and shown 1 photo at a time (15 total?) of dogs and are asked to rank them from 1-10 stars
+#user's choices are added to a list (??) 
+#at the end top 3 dogs ranked on the list are presented to the user
+#user is given an option to click on the top 3 dogs and find out more about them (card flip animation?)
+#user is presented with a button to click back to ask username page, and 'signed out'
