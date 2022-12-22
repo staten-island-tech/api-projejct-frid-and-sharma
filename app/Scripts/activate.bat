bd417cb46f899d8cf09209fcafeb308cc3243f74
Vfrid <113192881+Vfrid@users.noreply.github.com>
2022-12-22 10:32:38 -0500

    getting up to speed 12/22/22

--- app/Scripts/activate.bat
+++ app/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Yan.Sharma\Documents\GitHub\api-projejct-frid-and-sharma\app
+set VIRTUAL_ENV=C:\Users\Veronica.Frid\Documents\GitHub\api-projejct-frid-and-sharma\app
 
 if not defined PROMPT set PROMPT=$P$G
 
