f2ba60e5671eb8737bc88928446adf453467105c
Yan Sharma <yawning008@gmail.com>
2023-01-13 10:33:01 -0500

    Fixed errors

--- app/flaskr/testbreed.py
+++ app/flaskr/testbreed.py
@@ -6,5 +6,4 @@ response_API = requests.get('https://dog.ceo/api/breeds/list/all')
 data=response_API.text
 parse_json=json.loads(data)
 bre=parse_json["message"]
-thing=bre.keys()
-breeds=[item.capitalize() for item in thing]
+breeds=bre.keys()
