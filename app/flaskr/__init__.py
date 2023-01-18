61d3d8a3e519a0c891da353a5b498a963c089775
Vfrid <113192881+Vfrid@users.noreply.github.com>
2023-01-18 10:58:34 -0500

    1/18/23

--- app/flaskr/__init__.py
+++ app/flaskr/__init__.py
@@ -52,13 +52,6 @@ def create_app(test_config=None):
     @app.route('/history')
     def history():
         return render_template('history.html', img=img)
-    @app.route('/random')
-    def random():
-        response_API2 = requests.get(f'https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1')
-        data2=response_API2.text
-        parse_json2=json.loads(data2)
-        fact=parse_json2["fact"] 
-        return render_template('random.html', fact=fact)
 
     return app
 
